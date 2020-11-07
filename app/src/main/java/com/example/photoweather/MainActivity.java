eeb8104311553207737d4fa8924b4ef9ae0d91a6
Muhammed Taowfik <muhammedtaowfik@gmail.com>
2020-11-06 19:31:32 -0800

    add splash screen with animation

--- app/src/main/java/com/example/photoweather/MainActivity.java
+++ app/src/main/java/com/example/photoweather/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.photoweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.photoweather.utils.GuiManager;
+import com.example.photoweather.views.SplashScreenFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        GuiManager.getInstance().setFragmentManager(getSupportFragmentManager());
+        GuiManager.getInstance().setFragmentFrame(R.id.fragment_container);
+        GuiManager.getInstance().setCurrentFragment(new SplashScreenFragment(), 0, 0);
     }
 }
