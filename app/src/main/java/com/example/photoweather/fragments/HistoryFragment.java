bca91a947b4dc9e4cd29cf3e6a18ee27ff2a833e
Muhammed Taowfik <muhammedtaowfik@gmail.com>
2020-11-07 11:11:07 -0800

    add history fragment class

--- app/src/main/java/com/example/photoweather/fragments/HistoryFragment.java
+++ app/src/main/java/com/example/photoweather/fragments/HistoryFragment.java
@@ -1,4 +1,45 @@
 package com.example.photoweather.fragments;
 
-public class HistoryFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.photoweather.databinding.FragmentHistoryItemBindingImpl;
+import com.example.photoweather.databinding.FragmentHistoryListBinding;
+import com.example.photoweather.databinding.FragmentHomeBinding;
+import com.example.photoweather.viewmodels.HomeViewModel;
+
+public class HistoryFragment extends Fragment {
+
+    private FragmentHistoryListBinding binding;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        binding = FragmentHistoryListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 }
