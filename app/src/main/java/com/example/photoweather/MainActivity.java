df85bfa38049dea7c1c7864105abbb7f6cf8d2eb
Muhammed Taowfik <muhammedtaowfik@gmail.com>
2020-11-07 08:22:56 -0800

    apply data binding for home screen

--- app/src/main/java/com/example/photoweather/MainActivity.java
+++ app/src/main/java/com/example/photoweather/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.photoweather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.photoweather.viewmodels.HomeViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private HomeViewModel homeViewModel;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("key"); //if it's a string you stored.
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_bar);
+
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
+//        homeViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory
+//                .getInstance(this.getApplication())).get(HomeViewModel.class);
+//
+//        homeViewModel.getWeather().observe(this, new Observer<WeatherModel>() {
+//            @Override
+//            public void onChanged(WeatherModel weatherModel) {
+//                //update UI
+//            }
+//        });
+
+
+
     }
 }
